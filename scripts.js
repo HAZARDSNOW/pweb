4f8a4fc5e7e5d2859eb944729ce8187f29b4c55a
HAZARDSNOW <79214768+HAZARDSNOW@users.noreply.github.com>
2025-01-19 23:23:09 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -31,6 +31,8 @@ function setLanguage(lang) {
     localStorage.setItem('language', lang);
     document.body.classList.toggle('fa', lang === 'fa');
     document.querySelector('main').classList.toggle('fa', lang === 'fa');
+    document.querySelector('header').classList.toggle('fa', lang === 'fa');
+    document.querySelector('footer').classList.toggle('fa', lang === 'fa');
     updateContent(lang);
 }
 
@@ -38,6 +40,8 @@ function getLanguage() {
     const lang = localStorage.getItem('language') || 'en';
     document.body.classList.toggle('fa', lang === 'fa');
     document.querySelector('main').classList.toggle('fa', lang === 'fa');
+    document.querySelector('header').classList.toggle('fa', lang === 'fa');
+    document.querySelector('footer').classList.toggle('fa', lang === 'fa');
     updateContent(lang);
 }
 
